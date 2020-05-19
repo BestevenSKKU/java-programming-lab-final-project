e2470ecb9f53af622c7fd70016c0fcab2fa99a0b
BestevenSKKU <nikkuneimk@g.skku.edu>
2020-05-20 01:17:24 +0900

    [Init] game libgdx

---
+++ desktop/src/com/teameleven/javapracticelab/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.teameleven.javapracticelab.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.teameleven.javapracticelab.CrossingUsang;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CrossingUsang(), config);
+	}
+}
