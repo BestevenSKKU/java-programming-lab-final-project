dfffd756c77d627f769570473aa7e2f3c6693521
BestevenSKKU <nikkuneimk@g.skku.edu>
2020-05-19 22:58:24 +0900

    [Edit] Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("모여봐요 유생의 숲");
         System.out.println("달려봐요");
+        System.out.println("힘내봐요");
     }
 }
