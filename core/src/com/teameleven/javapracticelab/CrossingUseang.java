6ce193ab826fc01761b308d3cc1636060444b993
BestevenSKKU <nikkuneimk@g.skku.edu>
2020-05-20 02:13:02 +0900

    [Edit] Start page

--- core/src/com/teameleven/javapracticelab/CrossingUseang.java
+++ core/src/com/teameleven/javapracticelab/CrossingUseang.java
@@ -6,22 +6,22 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class CrossingUsang extends ApplicationAdapter {
+public class CrossingUseang extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("Intro.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor((float)(151/255.0), (float)(108/255.0), (float)(39/255.0), 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, (1024-639)/2, (768 - 246)/2);
 		batch.end();
 	}
 	
--- desktop/src/com/teameleven/javapracticelab/desktop/DesktopLauncher.java
+++ desktop/src/com/teameleven/javapracticelab/desktop/DesktopLauncher.java
@@ -2,11 +2,15 @@ package com.teameleven.javapracticelab.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.teameleven.javapracticelab.CrossingUsang;
+import com.teameleven.javapracticelab.CrossingUseang;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new CrossingUsang(), config);
+		config.title = "Usaseng Crossing";
+		config.useGL30 = true;
+		config.width = 1024;
+		config.height = 768;
+		new LwjglApplication(new CrossingUseang(), config);
 	}
 }
