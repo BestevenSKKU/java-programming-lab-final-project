b152dd608838ac2cf003dd3e65529ac376bb5b55
BestevenSKKU <nikkuneimk@g.skku.edu>
2020-05-20 01:31:42 +0900

    [Init] game libgdx

--- desktop/src/com/teameleven/javapracticelab/desktop/DesktopLauncher.java
+++ desktop/src/com/teameleven/javapracticelab/desktop/DesktopLauncher.java
@@ -6,6 +6,7 @@ import com.teameleven.javapracticelab.CrossingUsang;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		System.out.println("Hello, World!");
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new CrossingUsang(), config);
 	}
