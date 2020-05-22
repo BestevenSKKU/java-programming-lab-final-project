efa36ed61473165e8f69ade8d4813bb9edad5755
Su-young-MIn <65070885+Su-young-MIn@users.noreply.github.com>
2020-05-22 15:30:58 +0900

    test

--- desktop/src/com/teameleven/javapracticelab/desktop/DesktopLauncher.java
+++ desktop/src/com/teameleven/javapracticelab/desktop/DesktopLauncher.java
@@ -11,7 +11,7 @@ public class DesktopLauncher {
 		config.useGL30 = true;
 		config.width = 1024;
 		config.height = 768;
-		config.resizable = false;
+		config.resizable = true;
 		new LwjglApplication(new CrossingUsaseng(), config);
 	}
 }
