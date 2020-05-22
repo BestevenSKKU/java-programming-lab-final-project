85db0be88c4e826f4692645046c130c059f5c21e
현동 <@DESKTOP-P9F9ITI>
2020-05-22 15:36:08 +0900

    restore resize

--- desktop/src/com/teameleven/javapracticelab/desktop/DesktopLauncher.java
+++ desktop/src/com/teameleven/javapracticelab/desktop/DesktopLauncher.java
@@ -7,11 +7,11 @@ import com.teameleven.javapracticelab.CrossingUsaseng;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "¸ð¿©ºÁ¿ä À¯»ýÀÇ ½£";
+		config.title = "ï¿½ð¿©ºï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½";
 		config.useGL30 = true;
 		config.width = 1024;
 		config.height = 768;
-		config.resizable = true;
+		config.resizable = false;
 		new LwjglApplication(new CrossingUsaseng(), config);
 	}
 }
