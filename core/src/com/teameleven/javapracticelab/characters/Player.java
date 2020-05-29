c2c28cc5b2303bebb8fc07d95919fc266a31d8cc
BestevenSKKU <nikkuneimk@g.skku.edu>
2020-05-30 03:04:21 +0900

    [Edit] 움직임 자연스럽게

--- core/src/com/teameleven/javapracticelab/characters/Player.java
+++ core/src/com/teameleven/javapracticelab/characters/Player.java
@@ -9,8 +9,10 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 
-
 public class Player extends Sprite {
+    public enum Direction {
+        LEFT, RIGHT
+    }
     final private String name;
 
     private int speed = 5;
@@ -18,13 +20,11 @@ public class Player extends Sprite {
     float positionX = 0.0f;
     float positionY = 0.0f;
 
-    Texture img;
-    TextureRegion[] animationFrames;
-    Animation<TextureRegion> animation;
-    
-    Texture img2;
-    TextureRegion[] animationFrames2;
-    Animation<TextureRegion> animation2;
+    Texture[] img = new Texture[2];
+    TextureRegion[][] animationFrame = new TextureRegion[2][];
+    Animation<TextureRegion>[] animation = new Animation[2];
+
+    Direction previousDirection = Direction.LEFT;
     
     float elapsedTime;
     
@@ -32,35 +32,34 @@ public class Player extends Sprite {
         super(new Texture("player_m.png"));
         this.name = name;
         
-        img = new Texture("player_walk_l.png");
+        img[Direction.LEFT.ordinal()] = new Texture("player_walk_l.png");
+        img[Direction.RIGHT.ordinal()] = new Texture("player_walk_r.png");
+        TextureRegion[][][] tmpFrames = new TextureRegion[2][][];
+        tmpFrames[Direction.LEFT.ordinal()] = TextureRegion.split(
+                img[Direction.LEFT.ordinal()],
+                img[Direction.LEFT.ordinal()].getWidth() /2 ,
+                img[Direction.LEFT.ordinal()].getHeight() / 2
+        );
+        tmpFrames[Direction.RIGHT.ordinal()] = TextureRegion.split(
+                img[Direction.RIGHT.ordinal()],
+                img[Direction.RIGHT.ordinal()].getWidth() /2 ,
+                img[Direction.RIGHT.ordinal()].getHeight() / 2
+        );
 
-        TextureRegion[][] tmpFrames = TextureRegion.split(img,192,192);
+        animationFrame[Direction.LEFT.ordinal()] = new TextureRegion[4];
+        animationFrame[Direction.RIGHT.ordinal()] = new TextureRegion[4];
 
-        animationFrames = new TextureRegion[4];
         int index = 0;
 
         for (int i = 0; i < 2; i++){
-           for(int j = 0; j < 2; j++) {
-              animationFrames[index++] = tmpFrames[j][i];
+            for(int j = 0; j < 2; j++) {
+                animationFrame[Direction.LEFT.ordinal()][index] = tmpFrames[Direction.LEFT.ordinal()][j][i];
+                animationFrame[Direction.RIGHT.ordinal()][index++] = tmpFrames[Direction.RIGHT.ordinal()][j][i];
            }
         }
 
-        animation = new Animation<TextureRegion>(1f/4f,animationFrames);
-        
-        img2 = new Texture("player_walk_r.png");
-
-        TextureRegion[][] tmpFrames2 = TextureRegion.split(img2,192,192);
-
-        animationFrames2 = new TextureRegion[4];
-        index = 0;
-        for (int i = 0; i < 2; i++){
-           for(int j = 0; j < 2; j++) {
-              animationFrames2[index++] = tmpFrames2[j][i];
-           }
-        }
-
-        animation2 = new Animation<TextureRegion>(1f/4f,animationFrames2);
-        
+        animation[Direction.LEFT.ordinal()] = new Animation<TextureRegion>(1f/4f,animationFrame[Direction.LEFT.ordinal()]);
+        animation[Direction.RIGHT.ordinal()] = new Animation<TextureRegion>(1f/4f,animationFrame[Direction.RIGHT.ordinal()]);
     }
 
     public void action(Batch batch) {
@@ -73,7 +72,7 @@ public class Player extends Sprite {
     public void spriteControl() {
 
         int speed = 4;
-        int changed = 0;
+        boolean directionChanged = false;
         
         if (Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)) {
             speed += 2;
@@ -83,36 +82,40 @@ public class Player extends Sprite {
         }
 
         if(Gdx.input.isKeyPressed(Input.Keys.W)) {
-         	elapsedTime += Gdx.graphics.getDeltaTime();
-            this.setRegion(animation.getKeyFrame(elapsedTime,true));
+            elapsedTime += Gdx.graphics.getDeltaTime();
+            this.setRegion(animation[previousDirection.ordinal()].getKeyFrame(elapsedTime,true));
             positionY +=speed;
-            changed = 1;
+            directionChanged = true;
         }
 
         
         if(Gdx.input.isKeyPressed(Input.Keys.S)) {
          	elapsedTime += Gdx.graphics.getDeltaTime();
-            this.setRegion(animation.getKeyFrame(elapsedTime,true));
+            this.setRegion(animation[previousDirection.ordinal()].getKeyFrame(elapsedTime,true));
             positionY -=speed;
-            changed = 1;
+            directionChanged = true;
         }
  
 
         if(Gdx.input.isKeyPressed(Input.Keys.A)) {
         	elapsedTime += Gdx.graphics.getDeltaTime();
-            this.setRegion(animation.getKeyFrame(elapsedTime,true));
+        	previousDirection = Direction.LEFT;
+            this.setRegion(animation[previousDirection.ordinal()].getKeyFrame(elapsedTime,true));
             positionX -=speed;
-            changed = 1;
+            directionChanged = true;
         }
  
 
         if(Gdx.input.isKeyPressed(Input.Keys.D)) {
          	elapsedTime += Gdx.graphics.getDeltaTime();
-            this.setRegion(animation2.getKeyFrame(elapsedTime,true));
+         	previousDirection = Direction.RIGHT;
+            this.setRegion(animation[previousDirection.ordinal()].getKeyFrame(elapsedTime,true));
             positionX +=speed;
-            changed = 1;
+            directionChanged = true;
         }
         
-        if (changed == 0) {this.setRegion(animationFrames[0]);}
+        if (directionChanged == false) {
+            this.setRegion(animationFrame[previousDirection.ordinal()][0]);
+        }
     }
 }
