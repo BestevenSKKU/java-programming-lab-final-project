f1c71daca291df1df85866afd8e7c644dfd34ad3
Yanghyeondong <hdyang0686@naver.com>
2020-06-03 10:20:04 +0900

    [Add] 인벤토리 디자인 추가 (인벤토리 박스 및 과일 클래스 제작 예정)

--- core/src/com/teameleven/javapracticelab/screens/InventoryScreen.java
+++ core/src/com/teameleven/javapracticelab/screens/InventoryScreen.java
@@ -5,13 +5,16 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.teameleven.javapracticelab.UsasengCrossing;
 import com.teameleven.javapracticelab.utils.Skins;
@@ -20,8 +23,14 @@ public class InventoryScreen implements Screen {
 	
     final UsasengCrossing game;
     private Stage stage;
+    
     InitGameScreen initGameScreen;
-
+    SpriteBatch batch;
+    
+    private Texture texture;
+    private Label title;
+    int row_height = Gdx.graphics.getWidth() / 10;
+    
     public InventoryScreen(final UsasengCrossing game, final InitGameScreen initGameScreen) {
     	
     this.game = game;
@@ -29,6 +38,17 @@ public class InventoryScreen implements Screen {
 
     stage = new Stage(new ScreenViewport());
     Gdx.input.setInputProcessor(stage);
+    
+    batch = new SpriteBatch();
+    
+    texture = new Texture(Gdx.files.internal("inventory_img.png"));
+    
+    title = new Label("아이템 목록", Skins.korean, "black");
+    title.setSize(Gdx.graphics.getWidth(),row_height);
+    title.setPosition(0,600);
+    title.setAlignment(Align.center);
+    stage.addActor(title);
+    
     int buttonWidth = Gdx.graphics.getWidth() / 5;
     int buttonHeight = Gdx.graphics.getHeight() / 12;
     
@@ -53,6 +73,8 @@ public class InventoryScreen implements Screen {
     });
     //stage.addActor(back);
     
+    
+    
     }
     
     
@@ -65,6 +87,12 @@ public class InventoryScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor((float)(151/255.0), (float)(108/255.0), (float)(39/255.0), 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+
+        
+        batch.begin();
+        batch.draw(texture, 160, 30);
+        batch.end();
+        
         stage.act();
         stage.draw();
 
